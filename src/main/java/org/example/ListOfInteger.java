2a0789300da6a41de915c6ccd61e00c1281bfa7f
dovgan <a.n.dovgan@gmail.com>
2024-03-10 14:27:41 +0200

    useed Virtual Threads

--- src/main/java/org/example/ListOfInteger.java
+++ src/main/java/org/example/ListOfInteger.java
@@ -1,7 +1,11 @@
 package org.example;
 
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
+
+import static org.example.Main.logger;
 
 public class ListOfInteger {
     List<Integer> listNumber;
@@ -11,6 +15,36 @@ public class ListOfInteger {
                 .flatMap(line -> Arrays.stream(line.split(System.lineSeparator())))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+    public void getAll() {
+        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            List<Future<?>> futures = new ArrayList<>();
+            futures.add(executorService.submit(() -> executeAndTime(this::findMax, "Max")));
+            futures.add(executorService.submit(() -> executeAndTime(this::findMin, "Min")));
+            futures.add(executorService.submit(() -> executeAndTime(this::findMedian, "Median")));
+            futures.add(executorService.submit(() -> executeAndTime(this::findArithmetic, "Arithmetic")));
+            futures.add(executorService.submit(() -> executeAndTime(this::findLargestIncreasingSequence, "Largest Increasing Sequence")));
+            futures.add(executorService.submit(() -> executeAndTime(this::findLargestDecreasingSequence, "Largest Decreasing Sequence")));
+
+            for (Future<?> future : futures) {
+                future.get();
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            logger.log(Level.SEVERE, "Error executing tasks", e);
+        }
+    }
+
+
+    private <T> void executeAndTime(Callable<T> task, String taskResult) {
+        long startTime = System.nanoTime();
+        try {
+            T result = task.call();
+            long endTime = System.nanoTime();
+            System.out.println(taskResult + " = " + result);
+            System.out.println("Method " + taskResult + " execution time: " + (endTime - startTime) / 1_000_000 + " milliseconds");
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error executing task", e);
+        }
     }
 
     public int findMax() {
@@ -57,9 +91,10 @@ public class ListOfInteger {
     private List<Integer> getLongestSequence(List<Integer> numbers, Comparator<Integer> comparator) {
         List<Integer> longestSequence = new ArrayList<>();
         List<Integer> currentSequence = new ArrayList<>();
+        Integer previous = null;
 
         for (Integer number : numbers) {
-            if (currentSequence.isEmpty() || comparator.compare(number, currentSequence.getLast()) > 0) {
+            if (currentSequence.isEmpty() || comparator.compare(number, previous) > 0) {
                 currentSequence.add(number);
             } else {
                 if (currentSequence.size() > longestSequence.size()) {
@@ -68,6 +103,7 @@ public class ListOfInteger {
                 currentSequence.clear();
                 currentSequence.add(number);
             }
+            previous = number;
         }
 
         if (currentSequence.size() > longestSequence.size()) {
@@ -76,4 +112,5 @@ public class ListOfInteger {
 
         return longestSequence;
     }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
@@ -11,10 +10,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-    private static final Logger logger = Logger.getLogger(Main.class.getName());
+    static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) throws Exception {
         long startTime = System.nanoTime();
+
         Scanner scanner = new Scanner(System.in);
         Executor executor = new Executor();
         Properties properties = new Properties();
@@ -32,13 +32,26 @@ public class Main {
         String filename = properties.getProperty("filename");
         List<String> list = new FileReader(filename).readList();
         ListOfInteger listOfInteger = new ListOfInteger(list);
+        System.err.println("--------------------------------------------------------");
+        long startTimeFirst = System.nanoTime();
+        listOfInteger.getAll();
+        long endTimeFirst = System.nanoTime();
+        System.out.println("First  time : " + (endTimeFirst - startTimeFirst) / 1_000_000 + " milliseconds");
+        System.err.println("--------------------------------------------------------");
+        long startTimeSecond = System.nanoTime();
+
         Method[] methods = ListOfInteger.class.getMethods();
         for (Method method : methods) {
             if (method.getName().startsWith("find")) {
                 executor.executeMethod(listOfInteger, method.getName());
             }
         }
+        long endTimeSecond = System.nanoTime();
+        System.out.println("Second  time : " + (endTimeSecond - startTimeSecond) / 1_000_000 + " milliseconds");
+        System.err.println("--------------------------------------------------------");
         long endTime = System.nanoTime();
+
+
         System.out.println("Full  time : " + (endTime - startTime) / 1_000_000 + " milliseconds");
         System.out.println("Press Enter to exit");
         scanner.nextLine();
